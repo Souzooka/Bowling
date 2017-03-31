a2e8db70141914d27b994c586a05db0dfa8d5c7e
Dakota <bazookaharwell@gmail.com>
2017-03-31 09:19:58 -1000

    test and verify returns a score for no strikes/spares

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-function bowling(arr) {}
+function bowling(arr) {
+  var sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let score in arr[i]) {
+      sum += arr[i][score];
+    }
+  }
+
+  return sum;
+
+}
 
 module.exports = bowling;
--- test/app.spec.js
+++ test/app.spec.js
@@ -9,4 +9,13 @@ describe('Bowling Throw Simulator', () => {
     expect(bowling).to.be.a('function');
   });
 
+  it('should return a valid score for a game containing no strikes/spares', () => {
+    expect(bowling( [{throw1: 3, throw2: 4}, {throw1: 4, throw2: 5},
+                    {throw1: 2, throw2: 1}, {throw1: 1, throw2: 0},
+                    {throw1: 2, throw2: 3}, {throw1: 2, throw2: 3},
+                    {throw1: 2, throw2: 3}, {throw1: 2, throw2: 3},
+                    {throw1: 2, throw2: 3}, {throw1: 2, throw2: 3}] ))
+                    .to.be.equal(50);
+  });
+
 });
